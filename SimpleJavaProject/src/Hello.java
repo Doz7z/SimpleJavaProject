6bd8db91a4dfca533585cbb44a33df9c1fd37e42
Edgar <Edgar@Edgar-PC.fritz.box>
2017-01-23 00:07:24 +0100

    New Sysout Message Okay

--- SimpleJavaProject/src/Hello.java
+++ SimpleJavaProject/src/Hello.java
@@ -2,9 +2,13 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		
 		System.out.println("New Messsage");
 		System.out.println("New Messsage");
 		System.out.println("New Messsage");
+		
+		System.out.println("Okay");
+		
 	}
 
 }
